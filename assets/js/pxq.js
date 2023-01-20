8ff9082d05255f83d6e85d4cd53134ec972d5966
MeLlamoLeandro <lea_giffoni@hotmail.com>
2023-01-20 03:25:52 -0300

    agrego funcion pxq para Store

---
+++ assets/js/pxq.js
@@ -0,0 +1,36 @@
+function pxq1() {
+    const p = 599;
+    var q = document.getElementById('item1').value; //Obtiene el valor de unidades del Select
+    var resultado = p * q;
+    document.getElementById("total1").innerHTML = '$'+ resultado;
+}
+function pxq2() {
+    const p = 1199;
+    var q = document.getElementById('item2').value; //Obtiene el valor de unidades del Select
+    var resultado = p * q;
+    document.getElementById("total2").innerHTML = '$'+ resultado;
+}
+function pxq3() {
+    const p = 2949;
+    var q = document.getElementById('item3').value; //Obtiene el valor de unidades del Select
+    var resultado = p * q;
+    document.getElementById("total3").innerHTML = '$'+ resultado;
+}
+function pxq4() {
+    const p = 3199;
+    var q = document.getElementById('item4').value; //Obtiene el valor de unidades del Select
+    var resultado = p * q;
+    document.getElementById("total4").innerHTML = '$'+ resultado;
+}
+function pxq5() {
+    const p = 8299;
+    var q = document.getElementById('item5').value; //Obtiene el valor de unidades del Select
+    var resultado = p * q;
+    document.getElementById("total5").innerHTML = '$'+ resultado;
+}
+function pxq6() {
+    const p = 48699;
+    var q = document.getElementById('item6').value; //Obtiene el valor de unidades del Select
+    var resultado = p * q;
+    document.getElementById("total6").innerHTML = '$'+ resultado;
+}
